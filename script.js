58b0d94289c94fdacf6f447080ab886719aef0a2
tia0919 <tia0919@terpmail.umd.edu>
2024-05-10 01:51:16 -0400

    buttons

---
+++ script.js
@@ -0,0 +1,8 @@
+// Contact info & Copyright info button
+function showContactInfo() {
+    alert("Contact information: example@example.com");
+}
+
+function showCopyright() {
+    alert("Copyright © 2024 Password Manager. All rights reserved.");
+}
