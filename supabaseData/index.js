8232960a17f51d4ab70d46b721d55fba773c94c4
Sapochee <thompsonnguyen2113@gmail.com>
2024-05-18 18:52:08 -0400

    Fixed and re-added database

---
+++ supabaseData/index.js
@@ -0,0 +1,50 @@
+const supabaseClient = require('@supabase/supabase-js')
+const bodyParser = require('body-parser')
+const express = require('express')
+
+const app = express()
+const port = 3000
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'))
+
+const supabaseUrl = 'https://ucqcwfhauugcwaueziit.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjcWN3ZmhhdXVnY3dhdWV6aWl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYwNTQyNzgsImV4cCI6MjAzMTYzMDI3OH0.ohbk1W24Xr7FAbSsQrUE0iSBItjAEsoZeVurQ_afu6A'
+const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey)
+
+app.get('/passwords', async (req, res) => {
+    console.log('Attempting to get password data')
+
+
+    const { data, error } = await supabase
+        .from('generatedPasswords')
+        .select()
+    if(error) {
+        console.log('Error')
+        res.send(error)
+    } else {
+        res.send(data)
+    }
+})
+
+app.post('/passwords', async (req, res) => {
+    console.log('Adding password')
+    console.log(req.body)
+    var password = req.body.password;
+
+
+    const { data, error } = await supabase
+        .from('generatedPasswords')
+        .insert({ generated_password: "test" })
+        .select()
+
+    if(error) {
+        console.log('Error')
+        res.send(error)
+    } else {
+        res.send(data)
+    }
+})
+
+app.listen(port, () => {
+    console.log('APP WORKS')
+})
